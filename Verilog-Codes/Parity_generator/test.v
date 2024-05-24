5b95c2dadc6c76fed49fa7c73d8ab213eec8ff8d
ramya-251103 <134849283+ramya-251103@users.noreply.github.com>
2024-05-24 14:58:47 +0530

    Create test.v

---
+++ Verilog-Codes/Parity_generator/test.v
@@ -0,0 +1,24 @@
+`timescale 1s / 1ns
+ 
+module test;
+reg [3:0] D;
+wire P_even, P_odd ;
+
+parity_gen dut(D,P_even,P_odd);
+integer i;
+
+initial begin
+for(i=0;i<2**4;i=i+1)
+begin
+{D}=i;
+#1;
+end
+end
+
+initial begin
+$monitor("D=%b,P_even=%b,P_odd=%b",D,P_even,P_odd);
+end
+
+
+
+endmodule
